e8cede1f8346d59853f7499132a9ebec91c699f7
Jackson Shen <shenj7@rose-hulman.edu>
2023-01-02 12:36:53 -0500

    small fix

--- script_generator/script_output.py
+++ script_generator/script_output.py
@@ -10,6 +10,8 @@ def finish_simulation(filepath: str, population: str):
     """
     sample_size = 20  # sample size for sampleIndividuals
     return "10000 late() {\n" \
-        f"g = {population}.sampleIndividuals({sample_size}).genomes;\n" \
-        f"g.outputVCF(filePath=\"{filepath or uuid4()}\", simplifyNucleotides=T);\n" \
+        f"{population}.outputVCFSample(sampleSize={sample_size}, filePath=\"{filepath or uuid4()}\");" \
         "sim.simulationFinished();\n}\n"
+        # f"g = {population}.sampleIndividuals({sample_size}).genomes;\n" \
+        # f"g.outputVCF(filePath=\"{filepath or uuid4()}\", simplifyNucleotides=T);\n" \
+        # "sim.simulationFinished();\n}\n"
